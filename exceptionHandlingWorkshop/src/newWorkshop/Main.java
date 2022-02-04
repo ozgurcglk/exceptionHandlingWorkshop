9cb499aaf2d7b868607f0105153389f4b7bb4c9c
ozgurcglk <74430311+ozgurcglk@users.noreply.github.com>
2022-02-04 16:33:09 +0300

    Update Main.java

--- exceptionHandlingWorkshop/src/newWorkshop/Main.java
+++ exceptionHandlingWorkshop/src/newWorkshop/Main.java
@@ -9,9 +9,9 @@ public class Main {
 
 	public static void main(String[] args) throws BusinessException {
 		
-		User user1 = new User(1,"Zümra", "Girgin", "zmrgrgn@gmail.com", "1234576");
+		User user1 = new User(1,"ZÃ¼mra", "Girgin", "zmrgrgn@gmail.com", "1234576");
 		User user2 = new User(2,"Umut", "Taze", "umtz@gmail.com", "121566");
-		User user3 = new User(3,"Aaa", "Baa", "ozgurcglk@gmail.com", "112233");
+		User user3 = new User(3,"Ozgur", "Caglak", "ozgurcglk@gmail.com", "112233");
 		
 		UserManager2 userManager2 = new UserManager2(new UserHibernateDao());
 		
@@ -22,7 +22,7 @@ public class Main {
 		} catch (BusinessException e) {
 			System.out.println(e.getMessage());
 		} catch (Exception e) {
-			System.out.println("Bilinmeyen bir hata oluþtu");
+			System.out.println("Bilinmeyen bir hata oluÃ¾tu");
 		}
 		
 	}
